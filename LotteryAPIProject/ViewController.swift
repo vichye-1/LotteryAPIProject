1c3ebd35199e5244697a59d8a4b7fb31d5225df5
knockknock <yangban11111@gmail.com>
2024-06-05 15:40:29 +0900

    storyboard삭제, VC 기본 설정 - textfield

--- LotteryAPIProject/ViewController.swift
+++ LotteryAPIProject/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Alamofire
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    let lotteryTextField = UITextField()
+    let infoLabel = UILabel()
+    let dateLabel = UILabel()
+    let dividerView = UIView()
+    let resultLabel = UILabel()
+    let backgroundView = UIView()
+    let ball1Label = UILabel()
+    let ball2Label = UILabel()
+    let ball3Label = UILabel()
+    let ball4Label = UILabel()
+    let ball5Label = UILabel()
+    let ball6Label = UILabel()
+    let plusLabel = UILabel()
+    let ball7Label = UILabel()
+    let bonusLabel = UILabel()
+    let numberPicker = UIPickerView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureHierachy()
+        configureLayout()
+        configureUI()
     }
-
+    
+    func configureHierachy() {
+        view.addSubview(lotteryTextField)
+    }
+    
+    func configureLayout() {
+        lotteryTextField.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func configureUI() {
+        lotteryTextField.backgroundColor = .lightGray
+        
+    }
+    
 
 }
 
