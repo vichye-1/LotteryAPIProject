035a65ea6149f30dd7edba40a79bbb3d74c1bab4
knockknock <yangban11111@gmail.com>
2024-06-05 15:19:00 +0900

    Initial Commit

---
+++ LotteryAPIProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LotteryAPIProject
+//
+//  Created by 양승혜 on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
